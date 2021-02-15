bae758491f2979dcc8e750ea6ef093f497b67933
pav.ziaz@gmail.com <pav.ziaz@gmail.com>
2021-02-15 21:26:27 +0300

    Добавьте файлы проекта.

---
+++ Hubs/MainHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Hubs
+{
+    public class MainHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Net.Http;
+using System.Web.Http;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
