4d60dfc7a1056ed921eb57a61117dfc63f2f3341
pav.ziaz@gmail.com <pav.ziaz@gmail.com>
2021-02-16 12:29:42 +0300

    updqate 16.02

---
+++ Models/TextField.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class TextField
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int W { get; set; }
+        public int H { get; set; }
+        public string TextContent { get; set; }
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                TextField t = (TextField)obj;
+                return (X == t.X) && (Y == t.Y);
+            }
+        }
+    }
+}
